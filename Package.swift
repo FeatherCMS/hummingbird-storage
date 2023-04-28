c88de1dac18f0c6f732f7660526f7c99a59abb6e
Tibor Bodecs <mail.tib@gmail.com>
2023-04-28 22:03:20 +0200

    bump deps

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "hummingbird-storage",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
     ],
     products: [
         .library(name: "HummingbirdStorage", targets: ["HummingbirdStorage"]),
@@ -12,11 +12,12 @@ let package = Package(
         .library(name: "S3Driver", targets: ["S3Driver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git",from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.48.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
-        .package(url: "https://github.com/soto-project/soto.git", from: "6.5.0"),
+        .package(url: "https://github.com/soto-project/soto-core.git", from: "6.5.0"),
+        .package(url: "https://github.com/soto-project/soto.git", from: "6.6.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "1.4.0"),
         .package(url: "https://github.com/FeatherCMS/hummingbird-aws.git", branch: "main"),
     ],
     targets: [
@@ -28,24 +29,24 @@ let package = Package(
         .target(name: "LocalDriver", dependencies: [
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "Logging", package: "swift-log"),
-            .target(name: "HummingbirdStorage")
+            .target(name: "HummingbirdStorage"),
         ]),
         .target(name: "S3Driver", dependencies: [
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "Logging", package: "swift-log"),
             .product(name: "SotoS3", package: "soto"),
-            .target(name: "HummingbirdStorage")
+            .target(name: "HummingbirdStorage"),
         ]),
         .testTarget(name: "LocalDriverTests",
              dependencies: [
-                "HummingbirdStorage",
-                "LocalDriver",
-                .product(name: "HummingbirdFoundation", package: "hummingbird")
+                .target(name: "HummingbirdStorage"),
+                .target(name: "LocalDriver"),
+                .product(name: "HummingbirdFoundation", package: "hummingbird"),
         ]),
         .testTarget(name: "S3DriverTests",
              dependencies: [
-                "HummingbirdStorage",
-                "S3Driver"
+                .target(name: "HummingbirdStorage"),
+                .target(name: "S3Driver"),
         ]),
     ]
 )
